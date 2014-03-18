839306cf49e998a27791648be195a19587a0fd44
Mark Cotton <tifosigoon@gmail.com>
2014-03-18 21:06:53 +0000

    added a favicon

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ ejs.close = '}}';
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
